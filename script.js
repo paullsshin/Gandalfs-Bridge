53d247fff1503c416fa068ea84355fe3f3bdcfc0
paullsshin <paullsshin@hotmail.com>
2022-06-08 18:03:03 -0700

    added comments for help

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 // Assignment Code
 
 // things to store numbers
+var numbers = "1234567890".split("") 
+
+// or ["1","2","3","4","5","6","7","8","9","0"]
 
 // things to store special characters
+var characters = []
 
 // things to store lower case letters
 
 // things to store uppercase letters
 
 // what sorts oftasks does me code need to complete?
+
+// generate password
+
+function writePassword() {
+  // prompt the user for their password length and store it in a variable
+
+  // validate thepassword length and make sure it is a number between or equal to 8-128
+
+  // prompt the user to see if they want special characters and to store it in a variable
+}
 
 
 
@@ -25,3 +39,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// https://www.w3schools.com/jsref/jsref_concat_array.asp
+
+// function generatePassword() {
+//   if( true )
+//   return;
+// }
