e9b7e2f1735422379bd8242a80ae837630facc8b
paullsshin <paullsshin@hotmail.com>
2022-06-11 10:00:06 -0700

    added new files and code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Gandalfs-Bridge
 Password Generator for Challenge 2
+
+For this challenge the objective is to create a password generator using Javascript
+
--- script.js
+++ script.js
@@ -1,48 +1,107 @@
 // Assignment Code
+// event listener to generate button
+const generateBtn = document.getElementById('generateBtn')
+generateBtn.addEventListener('click', writePassword)
+// the special characters 
+const specialCharacters = "!@#$%^&*()";
 
-// things to store numbers
-var numbers = "1234567890".split("") 
-
-// or ["1","2","3","4","5","6","7","8","9","0"]
-
-// things to store special characters
-var characters = []
-
-// things to store lower case letters
-
-// things to store uppercase letters
-
-// what sorts oftasks does me code need to complete?
-
-// generate password
-
-function writePassword() {
-  // prompt the user for their password length and store it in a variable
-
-  // validate thepassword length and make sure it is a number between or equal to 8-128
-
-  // prompt the user to see if they want special characters and to store it in a variable
-}
-
-
-
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+// Places password in the input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+ 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Prompts that come up after you click generate password
+function generatePassword() {
+  var passwordLength = prompt("Desired character length of password between 8 and 128");
 
-// https://www.w3schools.com/jsref/jsref_concat_array.asp
+  var numbers = confirm("Do you desire numbers in your password?");
 
-// function generatePassword() {
-//   if( true )
-//   return;
-// }
+  var lowerCases = confirm("Do desire lowercases in your password?");
+
+  var upperCases = confirm("Do you desire uppercases in your password?");
+
+  var special = confirm("Do you desire special characters in your password?");
+
+  // minimum count for numbers, lower Cases, upper Cases & special characters
+  var minimumCount = 1;
+
+
+  // variable of empty minimums for numbers, lower Cases, upper Cases & special characters
+
+  var minimumNumbers = "2";
+  var minimumLowerCases = "2";
+  var minimumUpperCases = "2";
+  var minimumSpecialCharacters = "2";
+
+
+  // Functions that assist password generation
+  var functionArray = {
+    getNumbers: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
+
+    getLowerCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    },
+
+    getUpperCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    },
+
+    getSpecialCharacters: function() {
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+    }
+
+};
+
+
+// if statements to ensure user selected ok for all and uses empty minimums above
+  if (numbers === true) {
+    minimumNumbers = functionArray.getNumbers();
+    minimumCount++;
+
+  }
+
+  if (lowerCases === true) {
+    minimumLowerCases = functionArray.getLowerCases();
+    minimumCount++;
+
+  }
+
+  if (upperCases === true) {
+    minimumUpperCases = functionArray.getUpperCases();
+    minimumCount++;
+
+  }
+
+  if (special === true) {
+    minimumSpecialCharacters = functionArray.getSpecialCharacters();
+    minimumCount++;
+
+  }
+
+  // empty string variable utilised for loop below
+  var randomPasswordGenerated = "";
+
+  // loop gathering random characters
+  for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
+    var randomNumberPicked = Math.floor(Math.random() * 4);
+
+    randomPasswordGenerated += randomNumberPicked;
+
+  }
+
+  // ensures characters are added to password
+  randomPasswordGenerated += minimumNumbers;
+  randomPasswordGenerated += minimumLowerCases;
+  randomPasswordGenerated += minimumUpperCases;
+  randomPasswordGenerated += minimumSpecialCharacters;
+
+
+  return randomPasswordGenerated;
+
+}
