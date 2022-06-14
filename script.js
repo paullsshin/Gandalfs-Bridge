b282f687b52ee683932c84edafe148de3bf61a1d
paullsshin <paullsshin@hotmail.com>
2022-06-14 13:53:19 -0700

    added variables for each criteria

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Assignment Code
-// 1. prompt the suer for the password criteria
+var generateBtn = document.querySelector("#generate");
+
+var lengthsPeeps = 8;
+var everyPeeps = [];
+var numberPeeps = ["1","2","3","4","5","6","7","8","9","0"];
+var littlePeeps = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var bigPeeps = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialPeeps = ["~","`","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}",";",":","'",",","<",".",">","/","?"];
+
+
+function generatePassword() {
+  console.log("Let's get this!")
+};
+// 1. prompt the user for the password criteria
 //  a. password length 8 < 128
 //  b. lowercase, uppercase, numbers, and special characters
 // 2. validate input
@@ -17,7 +30,6 @@
 
 
 
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
