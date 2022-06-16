7481a602d7a10e1589d7d28e5f019042ce24df61
paullsshin <paullsshin@hotmail.com>
2022-06-15 21:35:37 -0700

    added to script

--- script.js
+++ script.js
@@ -8,13 +8,8 @@ var littlePeeps = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","
 var bigPeeps = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var specialPeeps = ["~","`","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}",";",":","'",",","<",".",">","/","?"];
 
-
-function generatePassword() {
-  console.log("Let's get this!")
-};
 // 1. prompt the user for the password criteria
 //  a. password length 8 < 128
-//  b. lowercase, uppercase, numbers, and special characters
 // 2. validate input
 // 3. generate password based on criteria
 // 4. display password on page
@@ -40,5 +35,29 @@ function writePassword() {
 
 }
 
+
+
+function getPrompts() {
+  everyPeeps = parseInt(prompt("How many characters would you like your password to be? Please choose from 8 to 128 characters. "));
+  if(isNaN(lengthsPeeps) || lengthsPeeps < 8 || lengthsPeeps > 128) {
+    alert("Character length must be a number between 8 and 128 digits. Please try again");
+    return false;
+  }
+if(confirm("Would you like numbers in your password? ")) {
+  everyPeeps = everyPeeps.concat(numberPeeps);
+}
+
+if(confirm("Would you like to have lower case letters in you password?")) {
+  everyPeeps = everyPeeps.concat(littlePeeps);
+}
+
+
+  
+  console.log("did this work?")
+  
+
+};
+
+getPrompts();
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
