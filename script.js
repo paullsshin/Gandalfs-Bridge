8fb5e9a83517c027207bfac89b845154e8370ba9
paullsshin <paullsshin@hotmail.com>
2022-06-24 17:33:31 -0700

    added final code to java

--- script.js
+++ script.js
@@ -1,32 +1,81 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-var lengthsPeeps = 8;
-var everyPeeps = [];
-var numberPeeps = ["1","2","3","4","5","6","7","8","9","0"];
-var littlePeeps = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var bigPeeps = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var specialPeeps = ["~","`","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}",";",":","'",",","<",".",">","/","?"];
+function generatePassword() {
+  // variables for each requirement
+  var numberPeeps = ["1","2","3","4","5","6","7","8","9","0"];
+  var littlePeeps = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var bigPeeps = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var specialPeeps = ["~","`","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}",";",":","'",",","<",".",">","/","?"];
+  var lengthPeeps = [];
+// prompts asking the use what they want in their password
+ characterLength = prompt("How many characters would you like to be in your password? Please choose a number between 8 and 128 chracters. ");
+  if (characterLength < 8 || characterLength > 128) {
+    return prompt("Please enter a valid number between 8 and 128.");
+  } else if (isNaN(characterLength)) {
+    characterLength = prompt("Please enter a number digit. ");
+  } else {
+    alert("Your password is going to be " + characterLength + " characters long.");
+  }
 
-// 1. prompt the user for the password criteria
-//  a. password length 8 < 128
-// 2. validate input
-// 3. generate password based on criteria
-// 4. display password on page
-// things to store numbers
+  lowerCase = confirm(("Would you like to have little letters in your password? "));
+  if (lowerCase) {
+    alert("You will have little letters.");
+  } else {
+    alert("Your password will not have little letters.");
+  }
 
-// things to store special characters
+  upperCase = confirm(("Would you like to have big letters in your password? "));
+  if (upperCase) {
+    alert("You will have big letters. ");
+  } else {
+    alert("Your password will not have big letters. ");
+  }
 
-// things to store lower case letters
+  storeNumbers = confirm(("Would you like to have numbers in your password? "));
+  if (storeNumbers) {
+    alert("Your password will have numbers. ");
+  } else {
+    alert("Your password will not have numbers. ");
+  }
 
-// things to store uppercase letters
+  makeSpecial = confirm(("Would you like to have special characters in your password? "));
+  if(makeSpecial) {
+    alert("Your password will have special characters. ");
+  } else {
+    alert("Your password will not have special characters.");
+  }
+// if no options are chosen then a message will pop up saying to choose at least one option
+  if (lowerCase === false && upperCase === false && storeNumbers === false && makeSpecial === false) {
+    return confirm("Please select at least one option for your password.");
+  };
+// once options are chosen they will be merged to create a password
+  if (lowerCase) {
+    lengthPeeps = lengthPeeps.concat(littlePeeps);
+  }
 
-// what sorts oftasks does me code need to complete?
+  if (upperCase) {
+    lengthPeeps = lengthPeeps.concat(bigPeeps);
+  }
 
+  if (storeNumbers) {
+    lengthPeeps = lengthPeeps.concat(numberPeeps);
+  }
 
+  if (makeSpecial) {
+    lengthPeeps = lengthPeeps.concat(specialPeeps);
+  }
 
-
-// Write password to the #password input
+  var getPassword = "";
+  for ( var i = 0; i < characterLength; i++) {
+    var rng = [Math.floor(Math.random() * lengthPeeps.length)];
+    getPassword = getPassword + lengthPeeps[rng];
+  }
+  // password will generate off of the given options
+  return getPassword;
+};
+// password will appear inside of the box
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -35,29 +84,5 @@ function writePassword() {
 
 }
 
-
-
-function getPrompts() {
-  everyPeeps = parseInt(prompt("How many characters would you like your password to be? Please choose from 8 to 128 characters. "));
-  if(isNaN(lengthsPeeps) || lengthsPeeps < 8 || lengthsPeeps > 128) {
-    alert("Character length must be a number between 8 and 128 digits. Please try again");
-    return false;
-  }
-if(confirm("Would you like numbers in your password? ")) {
-  everyPeeps = everyPeeps.concat(numberPeeps);
-}
-
-if(confirm("Would you like to have lower case letters in you password?")) {
-  everyPeeps = everyPeeps.concat(littlePeeps);
-}
-
-
-  
-  console.log("did this work?")
-  
-
-};
-
-getPrompts();
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
